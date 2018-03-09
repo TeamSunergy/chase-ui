582da2f21cba66a24dfeb4c9b688900cf81336f5
Steve <soaresss@appstate.edu>
2018-03-09 16:15:29 -0500

    add websocket

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
-var net = require('net');
-var client = new net.Socket();
+const net = require('net');
+const client = new net.Socket();
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const port = 4001;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server)
+
 client.connect(25000, '127.0.0.1', function() {
-	console.log('Connected @ localhost on Port: 25000');
-});
+		console.log('Connected @ localhost on Port: 25000');
+	});
 
-client.on('data', function(data) {
-	console.log('Received: ' + data);
+io.on('connection', socket => {
+  console.log('User connected');
+  client.on('data', function(data) {
+  	  var data = JSON.parse(data.toString());
+  	  data.time = new Date().toLocaleString();
+  io.sockets.emit("data", data);
 });
-
-client.on('close', function() {
-	console.log('Connection closed');
-});
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+server.listen(port, () => console.log("Listening on port: ", port))
