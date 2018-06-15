13552d96057af924400447530298326a0c2dba24
Steve <soaresss@appstate.edu>
2018-06-14 20:05:29 -0400

    Add ability to reestablish connection

--- src/server.js
+++ src/server.js
@@ -1,24 +1,55 @@
-const net = require('net');
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const port = 4001;
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-const Parser = require("json-stream-parse");
-const parser = new Parser();
-var socket = new net.Socket();
-var count = 0;
+const net = require('net'),
+	express = require('express'),
+	http = require('http'),
+	socketIO = require('socket.io'),
+	app = express(),
+	expressPort = 4001
+server = http.createServer(app),
+	io = socketIO(server),
+	Parser = require("json-stream-parse"),
+	parser = new Parser(),
+	log4js = require('log4js'),
+	logger = log4js.getLogger('chase-server'),
+	connection = false;
+var socket = new net.Socket(),
+	nconf = require('nconf');
+
+nconf.argv()
+	.env()
+	.file({ file: 'config/config.json' });
+
+logger.info(nconf.get("canpiServer"));
+logger.level = 'info';
+var canpiServer = nconf.get("canpiServer")
+
+establishConnection();
+
 io.once('connection', (socket) => {
+	logger.info('Event: SocketIO Connection Established w/ browser')
 	parser.on("json", (json) => {
 		io.sockets.emit("data", json);
-		console.log(json);
+		//logger.info(json)
 	});
 });
 
-socket.connect(25000, '127.0.0.1', () => {
-   	console.log('connection');
-	socket.pipe(parser);
-});
-server.listen(port, () => console.log("Listening on port: ", port));
+function establishConnection() {
+	socket.connect(canpiServer.port, canpiServer.host, () => {
+		connection = true;
+		logger.info("Connection made with host: " + canpiServer.host + " on port: " + canpiServer.port);
+		try {
+			socket.pipe(parser);
+		}
+		catch (e) {
+			logger.fatal("Could not pipe bytes to parser")
+		}
+	});
+	socket.on('error', function () { });
+
+	socket.on('close', () => {
+		connection = false;
+		logger.info("Socket closed - Attempting to establish a connection");
+		setTimeout(establishConnection, nconf.get("connectionInterval"));
+	});
+}
+
+server.listen(expressPort, () => console.log("Listening on port: ", expressPort));
